7e40c9d2f60ca733eab223ec30b3f9d71384f928
Aminemaalej <aminemaalej96@gmail.com>
2024-09-04 00:15:39 -0400

    Cloudwatch alarms

---
+++ Terraform/ec2/cloudwatch.tf
@@ -0,0 +1,52 @@
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "scale-up"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.wordpress_asg.name
+}
+
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "scale-down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.wordpress_asg.name
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu" {
+  alarm_name                = "high-cpu-alarm"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = 2
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = 300
+  statistic                 = "Average"
+  threshold                 = 80
+  alarm_description         = "Alarm when CPU exceeds 80%"
+  dimensions                = {
+    AutoScalingGroupName = aws_autoscaling_group.wordpress_asg.name
+  }
+
+  alarm_actions             = [aws_autoscaling_policy.scale_up.arn]
+  ok_actions                = [aws_autoscaling_policy.scale_down.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "moderate_cpu" {
+  alarm_name                = "moderate-cpu-alarm"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = 2
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = 300
+  statistic                 = "Average"
+  threshold                 = 50
+  alarm_description         = "Alarm when CPU exceeds 50%"
+  dimensions                = {
+    AutoScalingGroupName = aws_autoscaling_group.wordpress_asg.name
+  }
+
+  alarm_actions             = [aws_autoscaling_policy.scale_up.arn]
+  ok_actions                = [aws_autoscaling_policy.scale_down.arn]
+}
--- Terraform/tfvars/staging/eu-west-1.tfvars
+++ Terraform/tfvars/staging/eu-west-1.tfvars
@@ -2,9 +2,9 @@ aws_region = "eu-west-1"
 
 ### EC2
 
-ec2_asg_desired_capacity = 1
+ec2_asg_desired_capacity = 2
 ec2_asg_min_size         = 1
-ec2_asg_max_size         = 1
+ec2_asg_max_size         = 4
 
 ### RDS
 
